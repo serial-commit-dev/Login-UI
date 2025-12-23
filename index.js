a73097029c10dfe6806068a3f5d137f201f76ff8
Neeraj S <neerajms2009@gmail.com>
2025-12-23 23:58:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function clearForm(){
+    document.getElementById('username-id').value = '';
+    document.getElementById('password-id').value = '';
+}
+
+
+function Submit(){
+  Username = getElementById('username-id').value = 1;
+
+}
+
