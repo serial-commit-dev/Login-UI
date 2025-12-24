e4248f836a690c9ef18b1bc891f0166a1ae9dd08
Neeraj S <neerajms2009@gmail.com>
2025-12-24 15:13:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function clearForm(){
-    document.getElementById('username-id').value = '';
-    document.getElementById('password-id').value = '';
+const userPassword = "2009";
+const userName = "fly-monkey";
+
+function Submit() {
+    let Username = document.getElementById('username-id').value;
+    let Password = document.getElementById('password-id').value;
+
+
+    if (Username === userName && Password === userPassword) {
+        alert("Logged In!");
+
+    }if (Username !== userName && Password !== userPassword) {
+        document.getElementById('username-id').value = "Incorrect Username!";
+        document.getElementById('password-id').value = "Incorrect Password!";
+        
+
+    }if (Username !== userName && Password === userPassword){
+        document.getElementById('username-id').value = "Incorrect Username!";
+
+
+    }if (Password !== userPassword && Username === userName){
+
+        document.getElementById('password-id').value = "Incorrect Password!";
+        
+    }
 }
 
 
-function Submit(){
-  Username = getElementById('username-id').value = 1;
-
+function clearForm(){
+    document.getElementById('username-id').value = "";
+    document.getElementById('password-id').value = "";
 }
 
